aa31be2bbe4f4f2238a3243ff595c748c6f5d879
Andreas Hansson <andreas.hansson@icomera.com>
2019-08-20 16:46:49 +0200

    Lesson 2 done

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
-cp index.html dist/index.html
-sass scss/style.scss dist/css/style.css
+cp src/index.html dist/index.html
+cp src/register.html dist/
+cp src/profiles.html dist/
+cp src/login.html dist/
+cp src/dashboard.html dist/
+
+sass src/scss/style.scss dist/css/style.css
 docker build -t scss-traversy:latest .
